958de4d717b2401f5df95453c604d985012abfa3
Yuri De Oliveira Monteiro <yude-oli@c2r10s1.42lisboa.com>
2024-02-10 16:02:07 +0000

    create basic files, and include libft

---
+++ includes/minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yude-oli <yude-oli@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 15:57:03 by yude-oli          #+#    #+#             */
+/*   Updated: 2024/02/10 15:59:04 by yude-oli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+// # include "history.h"
+# include "libft/libft.h"
+// # include "readline.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
+
+#endif
